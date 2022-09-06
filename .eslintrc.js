35542f8aef5fa802e5dd9dac217043f6018a30d1
Lucas Lima <lucas.lima@mundiale.com.br>
2022-09-06 10:22:35 -0300

    feat: configuração eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,10 @@
 module.exports = {
   env: {
+    'jest/globals': true,
     browser: true,
     commonjs: true,
     es2021: true
+
   },
   extends: [
     'standard'
@@ -10,6 +12,12 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 'latest'
   },
+  plugins: ['jest'],
   rules: {
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'warn',
+    'jest/valid-expect': 'error'
   }
 }
